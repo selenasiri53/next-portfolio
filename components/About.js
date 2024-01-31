0faa6c5655068e56fbfe4788496fae4fb7ca7d33
selenasiri53 <selenasiri53@gmail.com>
2024-01-31 15:14:19 -0500

    new cv

--- components/About.js
+++ components/About.js
@@ -149,7 +149,7 @@ const About = () => {
 
               <a
                 className="button group mt-30 overflow-hidden inline-block leading-lh-1.4 rounded-30 text-ellipsis text-center align-middle select-none transition-all duration-250 ease-in-out uppercase no-underline relative z-10 py-16 pr-70 pl-35 text-fs-15 font-semibold text-white bg-transparent outline-0 before:absolute before:-z-10 before:left-0 before:right-0 before:top-0 before:bottom-0 before:translate-x-full hover:before:translate-x-0 before:transition before:duration-300 before:ease-out"
-                href="assets/Selena_Siri_Full_Stack_Engineer.pdf"
+                href="assets/Selena_Siri_Full_Stack_Engineer_Resume_2024.pdf"
               >
                 <span
                   className={`relative z-20 text-${
